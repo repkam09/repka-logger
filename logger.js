c8785f485a26e453186a58a896469558c122adf6
Mark Repka <mark@repkam09.com>
2018-09-13 15:20:15 -0400

    Initial test version of simple logger

---
+++ logger.js
@@ -0,0 +1,54 @@
+const { createLogger, format, transports } = require("winston");
+const { combine, timestamp, label, prettyPrint } = format;
+
+/**
+ * This class implements a generic logger on top of Winston
+ * this should be used for all vgr-* projects to log to the central logging server
+ */
+class RepkaNodeLogger {
+
+    /**
+     * 
+     * @param {string} server the hostname of the server where logs are reported
+     * @param {string} path the api path of the server where logs are reported
+     * @param {string} logfile path to a log file that should be appended to
+     * @param {string} textlabel a prefix label for this logger
+     * @param {boolearn} ssl should ssl be used for the network logger
+     */
+    constructor(server, path, logfile, textlabel = "logger", ssl = true) {
+
+        const logger = createLogger({
+            level: "debug",
+            format: combine(timestamp(), label({ label: textlabel }), prettyPrint()),
+            transports: [
+
+            ]
+        });
+
+        if (server && path) {
+            if (ssl) {
+                logger.add(new transports.Http({ host: server, port: 443, path: path, ssl: true }));
+            } else {
+                logger.add(new transports.Http({ host: server, port: 80, path: path, ssl: false }));
+            }
+        }
+
+        if (logfile) {
+            logger.add(new transports.File({ filename: logfile, format: format.simple() }));
+        }
+
+        this.logger = logger;
+        return logger;
+    }
+
+    sendRaw(object) {
+        try {
+            this.logger.log(object);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+}
+
+module.exports = RepkaNodeLogger;
