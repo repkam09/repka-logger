bd79282665735decb0cad46ae647f9afdddcc6c9
Mark Repka <mark@repkam09.com>
2018-09-20 14:48:52 -0400

    Update logger to be able to modify on the fly

--- logger.js
+++ logger.js
@@ -17,27 +17,73 @@ class RepkaNodeLogger {
      */
     constructor(server, path, logfile, textlabel = "logger", ssl = true) {
 
-        const logger = createLogger({
-            level: "debug",
-            format: combine(timestamp(), label({ label: textlabel }), prettyPrint()),
-            transports: [new transports.Console({ format: format.simple() })]
-        });
-
-        if (server && path) {
-            if (ssl) {
-                logger.add(new transports.Http({ host: server, port: 443, path: path, ssl: true }));
-            } else {
-                logger.add(new transports.Http({ host: server, port: 80, path: path, ssl: false }));
-            }
-        }
-
-        if (logfile) {
-            logger.add(new transports.File({ filename: logfile, format: format.simple() }));
-        }
+        const logger = createWinstonLogger(server, path, logfile, textlabel, ssl);
 
         this.logger = logger;
-        return logger;
+
+        this.server = server;
+        this.path = path;
+        this.logfile = logfile;
+        this.textlabel = textlabel;
+        this.ssl = ssl;
     }
+
+    log(settings) {
+        return this.logger.log(settings);
+    }
+
+    verbose(message) {
+        return this.logger.log({ level: "verbose", message: message });
+    }
+
+    info(message) {
+        return this.logger.log({ level: "info", message: message });
+    }
+
+    error(message) {
+        return this.logger.log({ level: "error", message: message });
+    }
+
+    debug(message) {
+        return this.logger.log({ level: "verbose", message: message });
+    }
+
+    warn(message) {
+        return this.logger.log({ level: "warn", message: message });
+    }
+
+    setPrefix(prefix) {
+        this.previous = this.logger;
+        this.logger = null;
+        try {
+            this.logger = createWinstonLogger(this.server, this.path, this.logfile, prefix, this.ssl);
+            this.previous = null;
+        } catch (error) {
+            this.logger = this.previous;
+        }
+    }
+}
+
+function createWinstonLogger(server, path, logfile, textlabel, ssl) {
+    const logger = createLogger({
+        level: "debug",
+        format: combine(timestamp(), label({ label: textlabel }), prettyPrint()),
+        transports: [new transports.Console({ format: format.simple() })]
+    });
+
+    if (server && path) {
+        if (ssl) {
+            logger.add(new transports.Http({ host: server, port: 443, path: path, ssl: true }));
+        } else {
+            logger.add(new transports.Http({ host: server, port: 80, path: path, ssl: false }));
+        }
+    }
+
+    if (logfile) {
+        logger.add(new transports.File({ filename: logfile, format: format.simple() }));
+    }
+
+    return logger;
 }
 
 module.exports = RepkaNodeLogger;
--- logtester.js
+++ logtester.js
@@ -2,20 +2,40 @@ const RLogger = require("./logger");
 
 console.log("Starting up log tester");
 const logger = new RLogger("log.repkam09.com", "/logger/stream", "repkadev.log", "repkadev");
+//const logger = new RLogger(null, null, "repkadev.log", "repkadev");
+
+logger.info("About to change prefix for test");
+logger.setPrefix("repkadev_initial_prefix");
+logger.info("info");
+logger.warn("warn");
+logger.error("error");
 
 setInterval(() => {
-    const rand = getRandomInt(3);
+    const rand = getRandomInt(5);
 
     if (rand === 0) {
+        console.log("info");
         logger.log({ level: "info", message: "this is a test info level message" });
     }
 
     if (rand === 1) {
+        console.log("warn");
         logger.log({ level: "warn", message: "this is a test warn level message" });
     }
 
     if (rand === 2) {
+        console.log("error");
         logger.log({ level: "error", message: "this is a test error level message" });
+    }
+
+    if (rand === 3) {
+        console.log("Changing to prefix 3");
+        logger.setPrefix("repkadev_3");
+    }
+
+    if (rand === 4) {
+        console.log("Changing to prefix 4");
+        logger.setPrefix("repkadev_4");
     }
 
 }, 2000);
